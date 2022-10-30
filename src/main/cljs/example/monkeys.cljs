1a1659dc0b7c8e3f5e8541402dc8ced49a2016a5
Yurii <juretz32@yandex.ru>
2022-10-30 17:44:44 +0300

    Significant rework

--- src/main/cljs/example/monkeys.cljs
+++ src/main/cljs/example/monkeys.cljs
@@ -24,38 +24,40 @@
             [:tr {:key i} (map (fn [i x] [:td {:key i} x]) (range) r)])])
 
 
-(defn render-monkeys []
-  (reset! timeout (js/setTimeout render-monkeys (/ 1000.0 @interval)))
-  (dom/render [monkeys] (js/document.getElementById "monkeys")))
+(defn start-monkeying []
+  (js/clearTimeout @timeout)
+  (when (> @interval 0)
+    (reset! timeout (js/setTimeout start-monkeying (/ 1000.0 @interval)))
+    (dom/render [monkeys] (js/document.getElementById "monkeys"))))
 
 
-(defn update-params [event]
-  (let [v (.. event -target -value)]
-    (reset! interval v)
-    (js/clearTimeout @timeout)
-    (render-monkeys)))
+(defn update-speed [x]
+  (reset! interval x)
+  (start-monkeying))
 
 
 (defn input []
   [:div
-   [:span
-    {:style {:font-size "0.7em"
-             :font-family "Consolas"}}
-    "Monkeying speed: " [:input {:type "range"
-                                 :min 0.2
-                                 :max 5
-                                 :step 0.2
-                                 :value @interval
-                                 :style {:width "25%"}
-                                 :on-change update-params}]]])
+   {:style {:font-size "0.7em"
+            :font-family "Consolas"}}
+   "Monkeying speed: "
+   [:span {:style {:font-weight 800}} @interval " 🐵/s"]
+   [:div
+    [:input {:type "range"
+             :min 0
+             :max 5
+             :step 0.2
+             :value @interval
+             :style {:width "25%"}
+             :on-change (fn [x] (-> x .-target .-value update-speed))}]]])
 
 
 (dom/render [:div
              [:h1 "The 38 Monkeys🐒"]
              [:p {:style {:font-size "0.5em"}} "Арт-объект"]
-             [:p [input]]
-             [:p#monkeys]]
+             [:p#monkeys]
+             [:p [input]]]
             (js/document.getElementById "app"))
 
 
-(render-monkeys)
+(start-monkeying)
