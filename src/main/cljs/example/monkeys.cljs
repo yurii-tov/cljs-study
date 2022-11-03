a4ffa0162cafda9272d098b86dfaf087cd7f6ff8
Yurii <juretz32@yandex.ru>
2022-11-03 11:10:52 +0300

    Less interactive, more realistic monkeys

--- src/main/cljs/example/monkeys.cljs
+++ src/main/cljs/example/monkeys.cljs
@@ -6,57 +6,39 @@
 (set! js/document.title "38 🐵")
 
 
-(def interval (r/atom 1))
+(defn rand-monkey []
+  (rand-nth ["🙈" "🙉" "🙊"]))
 
 
-(def timeout (atom nil))
+(def monkeys
+  (r/atom (->> rand-monkey
+               repeatedly
+               (take 38)
+               vec)))
 
 
-(defn monkeys []
-  [:table (for [[i r] (->> (partial rand-nth ["🙈" "🙉" "🙊"])
-                           repeatedly
-                           (mapcat (comp reverse vector) (repeat " "))
-                           (take 30)
-                           (partition 15)
-                           cycle
-                           (take 5)
-                           (map vector (range)))]
-            [:tr {:key i} (map (fn [i x] [:td {:key i} x]) (range) r)])])
+(defn monkeys-table []
+  [:table
+   [:tbody
+    (->> (map (comp second list) @monkeys (range))
+         (mapcat (fn [i] (list [:td (@monkeys i)] [:td " "])))
+         (partition 15)
+         (map (fn [i x] (vec (list* :tr {:key i} x))) (range))
+         doall)]])
 
 
 (defn start-monkeying []
-  (js/clearTimeout @timeout)
-  (when (> @interval 0)
-    (reset! timeout (js/setTimeout start-monkeying (/ 1000.0 @interval)))
-    (dom/render [monkeys] (js/document.getElementById "monkeys"))))
-
-
-(defn update-speed [x]
-  (reset! interval x)
-  (start-monkeying))
-
-
-(defn input []
-  [:div
-   {:style {:font-size "0.7em"
-            :font-family "Consolas"}}
-   "Monkeying speed: "
-   [:span {:style {:font-weight 800}} @interval " 🐵/s"]
-   [:div
-    [:input {:type "range"
-             :min 0
-             :max 5
-             :step 0.2
-             :value @interval
-             :style {:width "25%"}
-             :on-change (fn [x] (-> x .-target .-value update-speed))}]]])
+  (dotimes [i (count @monkeys)]
+    (letfn [(f []
+              (js/setTimeout f (rand-int 10000))
+              (swap! monkeys assoc i (rand-monkey)))]
+      (f))))
 
 
 (dom/render [:div
              [:h1 "The 38 Monkeys🐒"]
              [:p {:style {:font-size "0.5em"}} "Арт-объект"]
-             [:p#monkeys]
-             [:p [input]]]
+             [:p [monkeys-table]]]
             (js/document.getElementById "app"))
 
 
